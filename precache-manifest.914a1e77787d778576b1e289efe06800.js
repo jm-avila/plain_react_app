b51a2cf8e6d3f57a79481c81a0e0c4ff80cd5f61
jm-avila <jm.avila1309@icloud.com>
2020-06-25 12:08:29 +0200

    Updates

---
+++ precache-manifest.914a1e77787d778576b1e289efe06800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274bc6ccfd1c76249f9516b466d069aa",
+    "url": "/plain_react_app/index.html"
+  },
+  {
+    "revision": "f787fc4cbaa10779b054",
+    "url": "/plain_react_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11769d5c2982347bcd8c",
+    "url": "/plain_react_app/static/js/2.a517e89e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/plain_react_app/static/js/2.a517e89e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f787fc4cbaa10779b054",
+    "url": "/plain_react_app/static/js/main.d94e5f80.chunk.js"
+  },
+  {
+    "revision": "46204fb1fa4c08b11d50",
+    "url": "/plain_react_app/static/js/runtime-main.157bd445.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/plain_react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
